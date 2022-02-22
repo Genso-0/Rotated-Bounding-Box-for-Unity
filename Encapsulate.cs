031944d094df0abe98c76f0e970bb268530bfe54
Genso-0 <44528817+Genso-0@users.noreply.github.com>
2022-02-23 00:17:32 +0100

    Update Encapsulate.cs
    Refactored due to RBB change to static.

--- Encapsulate.cs
+++ Encapsulate.cs
@@ -1,4 +1,4 @@
-using System;
+ 
 using UnityEngine;
 
 namespace RotatedBoundingVolume
@@ -9,7 +9,7 @@ namespace RotatedBoundingVolume
     public static class Encapsulate
     {
         /// <summary>
-        /// Returns a Rotated Bounding Box that encapsulates mesh renderers in a hierarchy.
+        /// Returns a bounds that encapsulates mesh renderers in a hierarchy.
         /// </summary>
         /// <param name="t"></param>
         /// <param name="offset"></param>
@@ -27,36 +27,6 @@ namespace RotatedBoundingVolume
             t.rotation = currentRotation;
             mergedBounds.center -= t.position;//make sure we remove the transform's position offset from the RBB before using RBB further down the line. Weird things happen if you dont.
             return mergedBounds;
-        }
-        /// <summary>
-        /// Gets the corners suround the bounding volume with an offset equal to half the size of the perimeterRBB.
-        /// Think of it as placing smaller boxes at the corners of a bigger box.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <param name="rbb"></param>
-        /// <param name="edgeSize"></param>
-        /// <returns></returns>
-        public static Vector3[] GetBoundCorners(RBB rbb, Vector3 edgeSize)
-        {
-            var halfSizeY = edgeSize.y * .51f;
-            var halfSizeX = edgeSize.x * .51f;
-            var halfSizeZ = edgeSize.z * .51f;
-            var verts = rbb.GetVertices();
-
-            return new Vector3[8]
-            {
-            //lower corners
-             verts[0] + (rbb.Rotation * new Vector3(-halfSizeX, -halfSizeY, -halfSizeZ)),//back left lower corner
-             verts[1] + (rbb.Rotation * new Vector3(halfSizeX, -halfSizeY, -halfSizeZ)),//back right lower corner
-             verts[2] + (rbb.Rotation * new Vector3(halfSizeX, -halfSizeY, halfSizeZ)),//foward right lower corner
-             verts[3] + (rbb.Rotation * new Vector3(-halfSizeX, -halfSizeY, halfSizeZ)),//forward left lower corner
-
-            //upper corners
-             verts[4] + (rbb.Rotation * new Vector3(-halfSizeX, halfSizeY, -halfSizeZ)),//back left upper corner
-             verts[5] + (rbb.Rotation * new Vector3(halfSizeX, halfSizeY, -halfSizeZ)),//back right upper corner
-             verts[6] + (rbb.Rotation * new Vector3(halfSizeX, halfSizeY, halfSizeZ)),//forward right upper corner
-             verts[7] + (rbb.Rotation * new Vector3(-halfSizeX, halfSizeY, halfSizeZ)),//forward left upper corner
-            };
         } 
     }
 }
